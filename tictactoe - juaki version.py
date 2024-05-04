830fdac20d2442d66c19147ce5f5314ce890ca84
Jeanou Añazco <marcelo.anazco@ucsp.edu.pe>
2024-05-04 02:25:30 -0500

    mejora de diseño y corrección de errores

--- tictactoe - juaki version.py
+++ tictactoe - juaki version.py
@@ -10,14 +10,15 @@ def val(cord, c):
     try:
         return nint(cord.split(',')[c]) - 1
     except:
-        print ("Ingrese un valor válido")
+        print ("Error: uno de los valores ingresados no respeta los parámetros dados")
         return -1
 
-lista = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+matriz = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 a = randint(0, 1)
 x = 0
 p = 0
 q = 1
+print("TicTacToe - Juaki's version")
 while q == 1:
     e = 0
     if a == 0:
@@ -28,56 +29,59 @@ while q == 1:
         a -= 1
     if x == 0:
         print ("Empieza el jugador '" + b + "'")
-    print("="*48 + "\n" + "Tablero")
+    elif x > 0:
+        print("Turno del jugador '" + b + "'")
+    print("="*48 + "\n" + "[f]" + '    ' "Tablero")
     while p<3:
-        print("|", lista[p][0], "|", lista[p][1], "|", lista[p][2], "|")
+        print(' ' + str(p+1) + ' ' + "[", matriz[p][0], "][", matriz[p][1], "][", matriz[p][2], "]")
         p += 1
     p = 0
+    print('     ' + '1' + '    ' + '2' + '    ' + '3' + '  ' + '[c]')
     while True:
         cord = input("Jugador '" + b + "' ingrese las coordenadas en el formato (f,c): ")
         f = val(cord, 0)
-        if f == -2:
+        if f == -1 or f == -2:
             continue
         c = val(cord, 1)
-        if c == -2:
+        if c == -1 or c == -2:
             continue
-        elif f < 0 or f > 2 or c < 0 or c > 2:
-            print("Error: los valores ingresados exceden los parámetros dados (1-3)")
+        if f < 0 or f > 2 or c < 0 or c > 2:
+            print("Error: los valores ingresados exceden los parámetros dados")
             continue
-        elif f >= 0 and f < 3 and c >= 0 and c < 3 and lista[f][c] == " ":
-            lista[f][c:c+1] = b
+        elif f >= 0 and f < 3 and c >= 0 and c < 3 and matriz[f][c] == " ":
+            matriz[f][c:c+1] = b
             break
         elif b != " ":
             print("Error: este sitio ya está ocupado, por favor elija otra posición")
             continue
+    print()
     x += 1
-    for i in lista:
-        d1 = [lista[0][0], lista[1][1], lista[2][2]]
-        d2 = [lista[0][2], lista[1][1], lista[2][0]]
+    for i in matriz:
+        d1 = [matriz[0][0], matriz[1][1], matriz[2][2]]
+        d2 = [matriz[0][2], matriz[1][1], matriz[2][0]]
         if i == ["X", "X", "X"] or i == ["O", "O", "O"] or d1 == ["X", "X", "X"] or d1 == ["O", "O", "O"] or d2 == ["X", "X", "X"] or d2 == ["O", "O", "O"]:
-            print("="*48 + "\n" + "Tablero")
+            print("="*48 + "\n" + "    Tablero")
             while p<3:
-                print("|", lista[p][0], "|", lista[p][1], "|", lista[p][2], "|")
+                print("[", matriz[p][0], "][", matriz[p][1], "][", matriz[p][2], "]")
                 p += 1
-            print("¡El jugador" + " " + "'" + b + "'" + " " + "es el ganador de esta partida!")
+            print("¡El jugador '" + b + "' es el ganador de esta partida!")
             q = 0
             break
-        else:
-            break
     while e<3:
-        y = [lista[0][e], lista[1][e], lista[2][e]]
+        y = [matriz[0][e], matriz[1][e], matriz[2][e]]
         if y == ["X", "X", "X"] or y == ["O", "O", "O"]:
-            print("="*48 + "\n" + "Tablero")
+            print("="*48 + "\n" + "    Tablero")
             while p<3:
-                print("|", lista[p][0], "|", lista[p][1], "|", lista[p][2], "|")
+                print("[", matriz[p][0], "][", matriz[p][1], "][", matriz[p][2], "]")
                 p += 1
-            print("¡El jugador" + " " + "'" + b + "'" + " " + "es el ganador de esta partida!")
+            print("¡El jugador '" + b + "' es el ganador de esta partida!")
             q = 0
         e += 1
     if x == 9:
-        print("="*48 + "\n" + "Tablero")
+        print("="*48 + "\n" + "    Tablero")
         while p<3:
-            print("|", lista[p][0], "|", lista[p][1], "|", lista[p][2], "|")
+            print("[", matriz[p][0], "][", matriz[p][1], "][", matriz[p][2], "]")
             p += 1
         print("Empate, no hay ganadores")
         break
+print("\n" + "[made by marcyeah]" + "\n")
